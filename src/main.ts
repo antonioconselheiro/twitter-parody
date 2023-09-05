44838c5ba3e26e728f3c05578f2764f369567b79
António Conselheiro <canudos.sudestino@gmail.com>
2023-09-05 10:37:14 -0300

    incluindo humor e extensões

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,5 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+  .catch(err => console.error(err))
+  .finally(() => 'Ba Dum Tss!');
