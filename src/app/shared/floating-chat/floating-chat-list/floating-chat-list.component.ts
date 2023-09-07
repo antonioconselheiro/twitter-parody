f115719321384712b459867f3cef1e29c3ecafd1
António Conselheiro <canudos.sudestino@gmail.com>
2023-09-07 10:36:03 -0300

    importando svg e criando os componentes internos do chat flutuante

---
+++ src/app/shared/floating-chat/floating-chat-list/floating-chat-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tw-floating-chat-list',
+  templateUrl: './floating-chat-list.component.html',
+  styleUrls: ['./floating-chat-list.component.scss']
+})
+export class FloatingChatListComponent {
+
+}
