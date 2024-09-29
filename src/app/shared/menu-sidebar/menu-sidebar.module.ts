4d35d88d108725568df8e3faf012939be28bd416
António Conselheiro <anarcanudos@proton.me>
2024-09-29 13:50:28 -0300

    revisando estrutura de css

--- src/app/shared/menu-sidebar/menu-sidebar.module.ts
+++ src/app/shared/menu-sidebar/menu-sidebar.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CredentialManagerWidgetModule, ProfileWidgetModule } from '@belomonte/nostr-gui-ngx';
 import { PopoverWidgetModule } from '@shared/popover-widget/popover-widget.module';
+import { SvgLoaderModule } from '@shared/svg-loader/svg-loader.module';
 import { MenuActiveObservable } from './menu-active.observable';
 import { MenuSidebarMobileComponent } from './menu-sidebar-mobile/menu-sidebar-mobile.component';
 import { MenuSidebarMobileObservable } from './menu-sidebar-mobile/menu-sidebar-mobile.observable';
 import { MenuSidebarComponent } from './menu-sidebar/menu-sidebar.component';
 import { MobileBottomNavigationComponent } from './mobile-bottom-navigation/mobile-bottom-navigation.component';
-import { SvgLoaderModule } from '@shared/svg-loader/svg-loader.module';
 
 @NgModule({
   declarations: [
